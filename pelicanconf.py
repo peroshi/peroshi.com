06df7cd50c099fb6db63c1a3790132a1b1576b17
Perry Roper <perryroper@gmail.com>
2014-11-09 17:16:29 +0000

    Add base config.

---
+++ pelicanconf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+SITENAME = 'Peroshi'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = 'en'
+
+# We don't need feeds generated.
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+# URLs
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+
+THEME = 'theme'
---
+++ publishconf.py
@@ -0,0 +1,11 @@
+import os
+import sys
+sys.path.append(os.curdir)
+from pelicanconf import *
+
+SITEURL = 'https://peroshi.com'
+RELATIVE_URLS = False
+
+DELETE_OUTPUT_DIRECTORY = True
+
+# GOOGLE_ANALYTICS = ''
